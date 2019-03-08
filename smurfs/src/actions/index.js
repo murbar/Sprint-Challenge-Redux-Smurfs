17929c6d70f04a2742ca32e6a8962fdac656d94e
Joel Bartlett <joel@murbar.com>
2019-03-08 12:47:45 -0500

    Add addSmurf

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -17,20 +17,34 @@ import axios from 'axios';
 
 const endpoint = 'http://localhost:3333/smurfs';
 
-export const GET_SMURFS = 'GET_SMURFS';
+export const BUSY = 'BUSY';
 export const GET_SUCCESS = 'GET_SUCCESS';
 export const GET_FAILURE = 'GET_FAILURE';
-export const BUSY = 'BUSY';
+export const ADD_SUCCESS = 'ADD_SUCCESS';
+export const ADD_FAILURE = 'ADD_FAILURE';
 
 export const getSmurfs = () => dispatch => {
-  dispatch({ type: BUSY, status: 'Fetching smurfs' });
+  dispatch({ type: BUSY });
   axios
     .get(endpoint)
     .then(({ data }) => {
       dispatch({ type: GET_SUCCESS, smurfs: data });
     })
     .catch(err => {
-      // dispatch({type: GET_FAILURE, error: err })
+      dispatch({ type: GET_FAILURE, error: err });
       console.log(err);
+    });
+};
+
+export const addSmurf = smurf => dispatch => {
+  dispatch({ type: BUSY });
+  axios
+    .post(endpoint, smurf)
+    .then(({ data }) => {
+      dispatch({ type: ADD_SUCCESS, smurf: data });
+    })
+    .catch(err => {
+      const msg = err.response.data.Error;
+      dispatch({ type: ADD_FAILURE, error: msg });
     });
 };
