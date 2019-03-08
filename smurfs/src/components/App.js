fb205ffba808a3c641d662e749ea0a334b3f27ee
Joel Bartlett <joel@murbar.com>
2019-03-08 12:08:01 -0500

    Setup App

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getSmurfs } from '../actions';
+import SmurfList from './SmurfList';
 import './App.css';
 /*
  to wire this component up you're going to need a few things.
@@ -7,16 +10,30 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
   render() {
+    const { busy, error, smurfs } = this.props;
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <h1>Smurf Village</h1>
+        {busy && <div>Loading...</div>}
+        <SmurfList smurfs={smurfs} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    busy: state.busy,
+    error: state.error
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getSmurfs }
+)(App);
