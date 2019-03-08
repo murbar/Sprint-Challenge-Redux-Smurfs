225f47bb0f37757982568fb1dd78f8a1bdb05b59
Joel Bartlett <joel@murbar.com>
2019-03-08 12:27:47 -0500

    Hide delete button for now

--- smurfs/src/components/Smurf.jsx
+++ smurfs/src/components/Smurf.jsx
@@ -9,7 +9,7 @@ const Smurf = props => {
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
-      <button onClick={handleDelete}>Delete</button>
+      {/* <button onClick={handleDelete}>Delete</button> */}
     </div>
   );
 };
